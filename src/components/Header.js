21835101f9c207c83c82505e0aaaf045e4f87ceb
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-05-30 21:54:17 -0500

    add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
-function Header() {
+import Logo from '../images/logo.svg';
 
+function Header() {
+    return (
+        <header className='header'>
+            <img src={Logo} alt='logo' />
+            <span className='brand'>my travel journal.</span>
+        </header>
+    );
 }
 
 export default Header;
