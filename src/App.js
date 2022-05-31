1f1820a258d1613375bc795952ba1d0cbb9443ec
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-05-30 22:17:51 -0500

    add card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import Header from "./components/Header";
+import Card from "./components/Card";
 import data from "./data";
 
 function App() {
+    const cards = data.map(item => <Card {...item} />);
     return (
         <>
             <Header />
+            <section>
+                {cards}
+            </section>
         </>
     );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,32 @@
-function Card(props) {
+import { IconContext } from "react-icons";
+import { IoLocationSharp } from 'react-icons/io5';
 
+function Card(props) {
+    return (
+        <>
+            <div className="card">
+                <div className='img-parent'>
+                    <img src={`${props.imageUrl}`} alt={props.title} />
+                </div>
+                <div className="card-info">
+                    <div className='location-parent'>
+                        <IconContext.Provider value={{ className: "location-icon" }}>
+                            <IoLocationSharp />
+                        </IconContext.Provider>
+                        <span className="location">{props.location}</span>
+                        <a className='maps-link' href={props.googleMapsUrl}>View on Google Maps</a>
+                    </div>
+                    <p className='title'>{props.title}</p>
+                    <div>
+                        <span className='date'>{props.startDate} - </span>
+                        <span className='date'>{props.endDate}</span>
+                    </div>
+                    <p className='description'>{props.description}</p>
+                </div>
+            </div>
+            <hr />
+        </>
+    );
 }
 
 export default Card;
